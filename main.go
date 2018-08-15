0404673c803c126657ebc35f4687ce73ba14d796
Adaickalavan <adaickalavan@gmail.com>
2018-08-15 19:28:57 +0800

    Update

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -18,13 +20,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	run()
-	fmt.Println("yess")
+	log.Fatal(run())
+	fmt.Println("Program completed")
 }
 
 func makeMuxRouter() http.Handler {
 	muxRouter := mux.NewRouter()
-	// muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/definition", getDefinition).Methods("GET")
 	// muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
 	return muxRouter
 }
@@ -46,28 +48,44 @@ func run() error {
 	return nil
 }
 
-// func setupDB() {
-// 	// Setup connection to our postgresql database
-// 	connString := `user=postgres
-// 	password=1234
-// 	host=localhost
-// 	port=5432
-// 	dbname=peopleDatabase
-// 	sslmode=disable`
+func setupDB() {
+	// Setup connection to our postgresql database
+	connString := `user=postgres
+					password=1234
+					host=localhost
+					port=5432
+					dbname=dictionaryDatabase
+					sslmode=disable`
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		panic(err)
+	}
 
-// 	db, err := sql.Open("postgres", connString)
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	// Check whether we can access the database by pinging it
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
 
-// 	// Check whether we can access the database by pinging it
-// 	err = db.Ping()
-// 	if err != nil {
-// 		panic(err)
-// 	}
+	// Place our opened database into a `dbstruct` and assign it to `store` variable.
+	// The `store` variable implements a `Store` interface. The `store` variable was
+	// declared globally in `store.go` file.
+	store = &dbStore{db: db}
+}
 
-// 	// Place our opened database into a `dbstruct` and assign it to `store` variable.
-// 	// The `store` variable implements a `Store` interface. The `store` variable was
-// 	// declared globally in `store.go` file.
-// 	store = &dbStore{db: db}
-// }
+func getDefinition(w http.ResponseWriter, r *http.Request) {
+	// Retrieve people from postgresql database using our `store` interface variable's
+	// `func (*dbstore) GetPerson` pointer receiver method defined in `store.go` file
+	personList, err := store.GetPerson()
+
+	// Convert the `personList` variable to JSON
+	personListBytes, err := json.Marshal(personList)
+	if err != nil {
+		fmt.Println(fmt.Errorf("Error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Write JSON list of persons to response
+	w.Write(personListBytes)
+}
